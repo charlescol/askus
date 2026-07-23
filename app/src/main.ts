c8943e14aec446efd79d131b79b6358a1ebd78e0
charlescol <charlescol@hotmail.fr>
2026-07-24 00:01:30 +0200

    fix(app): listen broadcast url

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,40 +1,33 @@
-import { NestFactory } from "@nestjs/core";
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from "@nestjs/platform-fastify";
-import { ValidationPipe } from "@nestjs/common/pipes";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import AppModule from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common/pipes';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import AppModule from './app.module';
 
 async function bootstrap(): Promise<void> {
   /* AppConfiguration */
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({ bodyLimit: 50048576 })
-  );
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ bodyLimit: 50048576 }));
 
   /* Pipes Configuration */
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.setGlobalPrefix("api");
+  app.setGlobalPrefix('api');
 
   /* Swagger configuration */
   const swaggerConfig = new DocumentBuilder()
-    .setTitle("Project")
-    .setDescription("Project API")
-    .setVersion("1.0")
+    .setTitle('Project')
+    .setDescription('Project API')
+    .setVersion('1.0')
     .addBearerAuth()
     .build();
 
-  if (process.env.DISABLE_SWAGGER !== "true") {
+  if (process.env.DISABLE_SWAGGER !== 'true') {
     const document = SwaggerModule.createDocument(app, swaggerConfig);
-    SwaggerModule.setup("swagger", app, document);
+    SwaggerModule.setup('swagger', app, document);
   }
 
   /* Port Configuration */
-  if (process.env.DISABLE_APP_ADDRESS_LISTENING === "true")
-    await app.listen(process.env.PORT || 3000);
-  else await app.listen(process.env.PORT || 3000, process.env.HOSTNAME || "");
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
